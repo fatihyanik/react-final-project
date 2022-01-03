9718a25a2948c01269dfdc7438cd41dd21e77e6c
fatihyanik <fatihyanik07@gmail.com>
2022-01-03 15:02:18 +0100

    I created Posts.jsx and I will create Post.jsx

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,16 @@
+import Post from "./Post";
+
+function Posts() {
+    return (
+        <div className="flex flex-[9] flex-wrap m-5">
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+        </div>
+    )
+}
+
+export default Posts
