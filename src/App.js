a685f3f5fd1037ea7db630b98d6af277a90282e9
fatihyanik <fatihyanik07@gmail.com>
2022-01-03 14:48:14 +0100

    I added Header.jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from './components/Header';
 import TopBar from './components/TopBar';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <TopBar />
+      <Header />
     </div>
   );
 }
