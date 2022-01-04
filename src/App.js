58818addb2c4d65f371cd722af4ddbf8095f06e9
fatihyanik <fatihyanik07@gmail.com>
2022-01-04 13:02:07 +0100

    I added path, element and Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
-
-
-
+import "./App.css";
+import TopBar from "./components/TopBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Settings from "./pages/Settings";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      
-      
-     
+      <Router>
+        <TopBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post/:id" element={<Single />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
