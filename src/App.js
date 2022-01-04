d3f6dcbda1165aed1acd9a8c7702a1c01a226165
fatihyanik <fatihyanik07@gmail.com>
2022-01-04 11:23:27 +0100

    I added datas folder and inside the datas folder i added posts.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import TopBar from './components/TopBar';
 
 
 
+
+
 function App() {
   return (
     <div className="App">
-      <TopBar />
+      
+      
      
     </div>
   );
---
+++ src/datas/posts.js
@@ -0,0 +1,58 @@
+const allPosts = [
+  {
+    id: 1,
+    title: "Why does JavaScript important?",
+    image: "https://terminalroot.com.br/assets/img/js/js.jpg",
+    date: "15.01.2022",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, excepturi sed provident nam temporibus reprehenderit ullam laudantium exercitationem nisi, quis iure? Pariatur hic officiis reiciendis velit sapiente, eius, deserunt ipsam ipsum assumenda dicta repellat soluta sit adipisci porro repellendus voluptate cum minima incidunt amet exercitationem inventore autem perspiciatis quas voluptatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    theme: "IT",
+    author: "Fatih Yanik",
+  },
+  {
+    id: 2,
+    title: "Why does React important?",
+    image:
+      "https://th.bing.com/th/id/R.602c674bfe6dc6d91dbf36f04207a26c?rik=3g491NDAn%2fv4Zg&pid=ImgRaw&r=0",
+    date: "15.01.2022",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, excepturi sed provident nam temporibus reprehenderit ullam laudantium exercitationem nisi, quis iure? Pariatur hic officiis reiciendis velit sapiente, eius, deserunt ipsam ipsum assumenda dicta repellat soluta sit adipisci porro repellendus voluptate cum minima incidunt amet exercitationem inventore autem perspiciatis quas voluptatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    theme: "IT",
+    author: "Fatih Yanik",
+  },
+  {
+    id: 3,
+    title: "Why does HTML important?",
+    image:
+      "https://th.bing.com/th/id/R.6bca193311a6f97d0fbda2e0869d1c7c?rik=dZ0utbdnm2NiIA&pid=ImgRaw&r=0",
+    date: "15.01.2022",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, excepturi sed provident nam temporibus reprehenderit ullam laudantium exercitationem nisi, quis iure? Pariatur hic officiis reiciendis velit sapiente, eius, deserunt ipsam ipsum assumenda dicta repellat soluta sit adipisci porro repellendus voluptate cum minima incidunt amet exercitationem inventore autem perspiciatis quas voluptatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    theme: "IT",
+    author: "Fatih Yanik",
+  },
+  {
+    id: 4,
+    title: "Why does Bootstrap important?",
+    image:
+      "https://th.bing.com/th/id/OIP.wBc4B74fBCoU-yi_3-ZGLQHaD4?pid=ImgDet&rs=1",
+    date: "15.01.2022",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, excepturi sed provident nam temporibus reprehenderit ullam laudantium exercitationem nisi, quis iure? Pariatur hic officiis reiciendis velit sapiente, eius, deserunt ipsam ipsum assumenda dicta repellat soluta sit adipisci porro repellendus voluptate cum minima incidunt amet exercitationem inventore autem perspiciatis quas voluptatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    theme: "IT",
+    author: "Fatih Yanik",
+  },
+  {
+    id: 5,
+    title: "Why does Tailwind important?",
+    image:
+      "https://th.bing.com/th/id/R.26b73d7b0dc0fa3638ccfd786491aabe?rik=Ey9waWVk4EFT7Q&pid=ImgRaw&r=0",
+    date: "15.01.2022",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, excepturi sed provident nam temporibus reprehenderit ullam laudantium exercitationem nisi, quis iure? Pariatur hic officiis reiciendis velit sapiente, eius, deserunt ipsam ipsum assumenda dicta repellat soluta sit adipisci porro repellendus voluptate cum minima incidunt amet exercitationem inventore autem perspiciatis quas voluptatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    theme: "IT",
+    author: "Fatih Yanik",
+  },
+];
+
+export default allPosts;
