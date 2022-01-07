13a5da59817637e4699d0a412e4b16415e78d3ff
fatihyanik <fatihyanik07@gmail.com>
2022-01-07 09:35:31 +0100

    I deleted users.json i added allUsers.js file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import allPosts from "./datas/posts";
 import TopBar1 from "./components/TopBar1";
+import allUsers from "./datas/allUsers";
 
 
 
@@ -16,9 +17,13 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+  const [users,setUsers] = useState([]);
+
+  console.log(users);
 
   async function getPosts(){   
 
+    setUsers(allUsers)
     setPosts(allPosts);
 
   }
@@ -28,7 +33,6 @@ function App() {
   },[])
 
 
-  console.log(posts);
 
   
   return (
@@ -40,9 +44,9 @@ function App() {
         <Route path="/posts" element={<Home posts={posts} />} />
         <Route path="/register" element={currentUser.active ? <Home posts={posts} /> : <Register />} />
         <Route path="/login" element={currentUser.active ? <Home posts={posts} /> : <Login setCurrentUser={setCurrentUser} />} />
-        <Route path="/post/:id" element={<Single posts={posts} />} />
-        <Route path="/write" element={currentUser.active ? <Write /> : <Login setCurrentUser={setCurrentUser}  />} />
-        <Route path="/settings" element={currentUser.active ? <Settings currentUser={currentUser} /> : <Login setCurrentUser={setCurrentUser} />} />
+        <Route path="/post/:id" element={<Single currentUser={currentUser} posts={posts} setPosts={setPosts} />} />
+        <Route path="/write" element={currentUser.admin ? <Write posts={posts} setPosts={setPosts} /> : <Login setCurrentUser={setCurrentUser}  />} />
+        <Route path="/settings" element={currentUser.active ? <Settings currentUser={currentUser} users={users} setUsers={setUsers} /> : <Login setCurrentUser={setCurrentUser} />} />
       </Routes>
     </Router>
   );
---
+++ src/datas/allUsers.js
@@ -0,0 +1,44 @@
+const allUsers = [
+  {
+    id: 0,
+    name: "Fatih Yanik",
+    email: "fatih@gmail.com",
+    password: 1234,
+    image:
+      "https://bilder.t-online.de/b/89/05/62/58/id_89056258/c_Master-1-1-Large/tid_da/david-beckham-der-ex-fussballer-ist-fuer-seine-wechselnden-frisuren-bekannt-.jpg",
+    active: false,
+    admin: true,
+  },
+  {
+    id: 1,
+    name: "Ahmet Yanik",
+    email: "ahmet@gmail.com",
+    password: 1234,
+    image:
+      "https://img.minutoneuquen.com/u/fotografias/fotosnoticias/2020/3/23/228917.jpg",
+    active: false,
+    admin: false,
+  },
+  {
+    id: 2,
+    name: "Marlyn Monroe",
+    email: "marlyn@gmail.com",
+    password: 1234,
+    image:
+      "https://th.bing.com/th/id/OIP.rVqWrgcvmnVD2t0xV_A4YQHaJ-?pid=ImgDet&rs=1",
+    active: false,
+    admin: false,
+  },
+  {
+    id: 3,
+    name: "Tom Cruise",
+    email: "tom@gmail.com",
+    password: 1234,
+    image:
+      "https://lh5.googleusercontent.com/proxy/bMQcJCLaq4js3x72sWU-cqc6gaqwxAexN6l3ZuxGl8mLiLQjJZV0o8GdWZ4sf37iWjTzPi_ZJwXwSWv278tFJ4toiPVMyzDWEkqDmbVV2roSnPD5brL-NLgy7klnVQjHxFfH_BtxFfAaOXrQcoXkmJF158tTnmgDffRJ5lO5-dgxfy9Qhp4MQySA1rYZBpKavOgTNCXcj7z7JLnAfn_VzjLCWNy4yyT_BRowocFQo9bnfDyeOOEGwgkErUb95V9VQVUJQygDrxwazDY5iVWVpoC0",
+    active: false,
+    admin: false,
+  },
+];
+
+export default allUsers;
