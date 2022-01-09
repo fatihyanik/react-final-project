319d7191f288c91f3cb4cc59bfb39f0b28b2e00d
fatihyanik <fatihyanik07@gmail.com>
2022-01-09 10:57:20 +0100

    I changed Single.jsx

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,11 +1,66 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react/cjs/react.development";
+import Comment from "../components/Comment";
 import SinglePost from "../components/SinglePost";
 
 function Single({ posts, setPosts, currentUser }) {
-  console.log(posts);
+  const params = useParams();
+  const [postIndex, setPostIndex] = useState();
+
+  function addComment(e) {
+    e.preventDefault();
+
+    const contentInput = document.querySelector("#content");
+    const content = contentInput.value;
+
+    let filteredCommentPost;
+    let selectedIndex;
+
+    posts.forEach((post, index) => {
+      if (parseInt(params.id) === post.id) {
+        selectedIndex = index;
+
+        filteredCommentPost = post;
+      }
+    });
+
+    setPostIndex(selectedIndex);
+
+    console.log(currentUser);
+
+    const newPost = {
+      commentUser: currentUser.name ? currentUser.name : "Kayitli olmayan kisi",
+      commentDate: new Date().toDateString(),
+      commentContent: content,
+      commentImage: currentUser.image,
+    };
+    const aktuelPost = filteredCommentPost;
+
+    aktuelPost.comments.push(newPost);
+
+    const newALLPOST = [...posts];
+
+    setPosts(newALLPOST);
+  }
 
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       <SinglePost posts={posts} setPosts={setPosts} currentUser={currentUser} />
+      <form onSubmit={addComment} className="mt-5 flex flex-col">
+        <div>
+          <button className="bg-indigo-500 w-24">Add Comment</button>
+          <label className="my-[10px] mx-0">Content: </label>
+          <textarea
+            name="content"
+            id="content"
+            className="p-[10px] bg-white border rounded-[10px] focus:outline-none"
+            type="text"
+            placeholder="Enter your username..."
+            required
+          />
+        </div>
+      </form>
+      <Comment posts={posts} />
     </div>
   );
 }
