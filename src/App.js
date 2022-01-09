b7bd0d49dba91d927f5b68890bdb3c696592fe52
fatihyanik <fatihyanik07@gmail.com>
2022-01-09 11:05:07 +0100

    I added some components in App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,38 @@
+import TopBar from "./components/TopBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Settings from "./pages/Settings";
 import Single from "./pages/Single";
 import Write from "./pages/Write";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
+import {BrowserRouter as Router,Route,Routes,Navigate} from "react-router-dom";
+import { useState } from "react";
 import allPosts from "./datas/posts";
 import TopBar1 from "./components/TopBar1";
 import allUsers from "./datas/allUsers";
 
-
-
 function App() {
-
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([...allPosts]);
   const [currentUser, setCurrentUser] = useState({});
-  const [users,setUsers] = useState([]);
+  const [users, setUsers] = useState([...allUsers]);
 
-  console.log(users);
+  console.log(currentUser);
 
-  async function getPosts(){   
-
-    setUsers(allUsers)
-    setPosts(allPosts);
-
-  }
-
-  useEffect(()=>{
-      getPosts();
-  },[])
-
-
-
-  
   return (
     <Router>
-       <TopBar1 currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-    {/*   <TopBar currentUser={currentUser} setCurrentUser={setCurrentUser}/> */}
+      <TopBar1 currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      {/*   <TopBar currentUser={currentUser} setCurrentUser={setCurrentUser}/> */}
       <Routes>
         <Route exact path="/" element={<Home posts={posts} />} />
         <Route path="/posts" element={<Home posts={posts} />} />
-        <Route path="/register" element={currentUser.active ? <Home posts={posts} /> : <Register />} />
-        <Route path="/login" element={currentUser.active ? <Home posts={posts} /> : <Login setCurrentUser={setCurrentUser} />} />
+        <Route path="/register" element={currentUser.active ? (<Home posts={posts} />) : (<Register users={users} setUsers={setUsers} 
+               setCurrentUser={setCurrentUser} />)} />
+        <Route path="/login" element={currentUser.active ? (<Navigate to="/" />) : (<Login users={users} setCurrentUser={setCurrentUser} />)} />
         <Route path="/post/:id" element={<Single currentUser={currentUser} posts={posts} setPosts={setPosts} />} />
-        <Route path="/write" element={currentUser.admin ? <Write posts={posts} setPosts={setPosts} /> : <Login setCurrentUser={setCurrentUser}  />} />
-        <Route path="/settings" element={currentUser.active ? <Settings currentUser={currentUser} users={users} setUsers={setUsers} /> : <Login setCurrentUser={setCurrentUser} />} />
+        <Route path="/write" element={currentUser.admin ? (<Write posts={posts} setPosts={setPosts} />) : (<Login users={users} 
+               setCurrentUser={setCurrentUser} />)} />
+        <Route path="/settings" element={currentUser.active ? (<Settings currentUser={currentUser} setCurrentUser={setCurrentUser}
+               users={users} setUsers={setUsers} />) : (<Login users={users} setCurrentUser={setCurrentUser} />)}/>
       </Routes>
     </Router>
   );
