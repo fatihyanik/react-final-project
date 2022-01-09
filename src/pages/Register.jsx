419530fe34341d7f289e71401e47367e53e57c07
fatihyanik <fatihyanik07@gmail.com>
2022-01-09 10:56:10 +0100

    I changed register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,69 +1,42 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Register() {
-  const [users, setUsers] = useState([]);
+function Register({ users, setUsers }) {
+  const [person, setPerson] = useState({
+    id: users.length,
+    name: "",
+    email: "",
+    password: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC4BPQQmAhaOjJX5QGnLcj8spS7lpopLiW36_P8FTSH0mlazlJvkoQRCULvbRtHuEOJbE&usqp=CAU",
+    active: false,
+    admin: false,
+  });
 
-  async function getUsers() {
-    const users = await fetch("/users.json");
-    const userJSON = await users.json();
+  function inputChange(e) {
+    let dataVonInput = e.target.value;
 
-    setUsers(userJSON);
+    e.target.name === "password"
+      ? setPerson({ ...person, [e.target.name]: parseInt(dataVonInput) })
+      : setPerson({ ...person, [e.target.name]: dataVonInput });
   }
 
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  function registerPerson(e) {
+  function addNewPerson(e) {
     e.preventDefault();
+    const newArray = [...users, person];
+    setUsers(newArray);
 
-    const inputUserName = document.querySelector("#userName");
-    const inputEmail = document.querySelector("#email");
-    const inputPassword = document.querySelector("#password");
-    const warning = document.querySelector("#warning");
-
-    const userName = inputUserName.value;
-    const userEmail = inputEmail.value;
-    const userPassword = inputPassword.value;
-
-    const bulunanKisi = users.filter((user, index) => {
-      return (
-        user.name === userName &&
-        user.email === userEmail &&
-        user.password === parseInt(userPassword)
-      );
-    });
-
-    if (bulunanKisi.length > 0) {
-      console.log(warning);
-      warning.setAttribute("class", "text-black block");
-    } else {
-      async function jsonVeriEkle() {
-        const response = await fetch("/users.json", {
-          method: "POST",
-          body: JSON.stringify({
-            title: "Fatih",
-            body: "Yanik",
-            userId: 5,
-          }),
-          headers: { "Content-Type": "application/json" },
-        });
-
-        const sonuc = await response.json();
-        console.log(sonuc);
-      }
-
-      jsonVeriEkle();
-    }
+    alert("You are registered!");
   }
 
   return (
     <div className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-r from-gray-300 to-gray-300 bg-cover">
       <span className="mt-[80px] text-[50px]">Register</span>
-      <form onSubmit={registerPerson} className="mt-5 flex flex-col">
+      <form onSubmit={addNewPerson} className="mt-5 flex flex-col">
         <label className="my-[10px] mx-0">Username</label>
         <input
+          onChange={inputChange}
+          name="name"
           id="userName"
           className="p-[10px] bg-white border-none rounded-[10px] focus:outline-none"
           type="text"
@@ -72,6 +45,8 @@ function Register() {
         />
         <label className="my-[10px] mx-0">Email</label>
         <input
+          onChange={inputChange}
+          name="email"
           id="email"
           className="p-[10px] bg-white border-none rounded-[10px] focus:outline-none"
           type="text"
@@ -80,6 +55,8 @@ function Register() {
         />
         <label className="my-[10px] mx-0">Password</label>
         <input
+          onChange={inputChange}
+          name="password"
           id="password"
           className="p-[10px] bg-white border-none rounded-[10px] focus:outline-none"
           type="password"
@@ -90,7 +67,7 @@ function Register() {
           Register
         </button>
       </form>
-      <button className="absolute top-[100px] right-[20px] bg-red-400 cursor-pointer p-[10px] border-none text-white rounded-[10px]">
+      <button className="absolute bottom-[60px] right-[20px] bg-red-400 cursor-pointer p-[10px] border-none text-white rounded-[10px]">
         <Link className="link" to="/login">
           Login
         </Link>
